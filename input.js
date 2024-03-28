80c3cb09a3532f55677e01ad104275981759ddc8
Sapphire7x <78709769+Sapphire7x@users.noreply.github.com>
2024-03-27 18:41:28 -0700

    Testing JSON scraping
    We'll see if this works

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-let input;
+//get words
+let letters = ['A', 'C', 'D', 'E', 'F', 'G', 'H']; //where first index is the required letter
+let wordsPossible = [];
+let wordsGotten = [];
 let displayedInput = document.getElementById('guess').innerHTML;
 let form = document.getElementById("spellingBee");
 let result = document.getElementById("result");
@@ -9,10 +12,34 @@ function checkWord(event) {
     let regex = /^[a-zA-Z]+$/;
     if(regex.test(word)) {
         //It is a valid string
-        if(word.length < 4) {
+        if(word.length < 4) { //if too short
             result.innerHTML = "necesse est longior!"
         } else {
-            //if in latin
+            let valid = true;
+            for(i of word) {
+                if (letters.indexOf(i) < 0) { //if uses illegal letters
+                    result.innerHTML = "invalidus!"
+                    valid = false;
+                }
+            }
+            if(word.indexOf(letters[0]) < 0) { //if doesn't have required letter
+                result.innerHTML = "necesse est habet " + letters[0] + "!";
+            } else if(wordsGotten.indexOf(word) < 0) { //if already guessed
+                result.innerHTML = "iterum?";
+            } else if(valid) { // if valid
+                fetch(`https://raw.githubusercontent.com/IohannesArnold/lewis-short-json/master/ls_${word[0]}.json`)
+                    .then((res) => {
+                        if (!res.ok) {
+                            throw new Error
+                                (`HTTP error! Status: ${res.status}`);
+                        }
+                        return res.json();
+                    })
+                    .then((data) => 
+                        console.log(data))
+                    .catch((error) => 
+                        console.error("Unable to fetch data:", error));
+            }
         }
     } else {
         //Invalid input
